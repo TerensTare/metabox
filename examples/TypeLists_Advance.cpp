c5a6f9c326e4d4fd5ebbe3e3b5b8263f15168bf8
Terens Tare <terens.t17@gmail.com>
2021-01-18 21:33:55 +0100

    Fixed small typos in an example

--- examples/TypeLists_Advance.cpp
+++ examples/TypeLists_Advance.cpp
@@ -5,15 +5,15 @@ int main()
 {
     constexpr auto tl = tnt::make_list<int, char, float, bool>;
 
-    constexpr auto fundamental = tnt::bind_value_fn<std::is_fundamental>;
-    constexpr auto integral = tnt::bind_value_fn<std::is_integral>;
-    constexpr auto reference = tnt::bind_value_fn<std::is_reference>;
+    constexpr auto fundamental = tnt::value_fn<std::is_fundamental>;
+    constexpr auto integral = tnt::value_fn<std::is_integral>;
+    constexpr auto reference = tnt::value_fn<std::is_reference>;
 
     static_assert(tnt::tl::all_of(fundamental, tl));
     static_assert(tnt::tl::any_of(integral, tl));
     static_assert(tnt::tl::none_of(reference, tl));
 
-    constexpr auto add_ref = tnt::bind_type_fn<std::add_lvalue_reference>;
+    constexpr auto add_ref = tnt::type_fn<std::add_lvalue_reference>;
 
     static_assert(tnt::tl::transform(add_ref, tl) == tnt::make_list<int &, char &, float &, bool &>);
     static_assert(tnt::tl::reverse(tl) == tnt::make_list<int, char, float, bool>);
